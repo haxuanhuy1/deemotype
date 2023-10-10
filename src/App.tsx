33d8c9555aebed9be12294dffbb8a5c8906d8907
haxuanhuy1 <huyha866@gmail.com>
2023-10-10 15:49:36 +0700

    l1

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,62 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+ 
+  class Person {
+    items: string;
+    train: string;
+    // khai báo ttrong typescript
+    constructor(firstName: string, lastName: string) {
+        this.items = firstName;
+        this.train = lastName;
+    }
+
+    sayHello(): void {
+        console.log(this);
+    }
+}
+class Mercedes {
+  colors:string
+  names : string
+  weights : number
+  constructor(color:string , name:string , weight:number) {
+    this.colors = color;
+    // ở đây bạn đang gán giá trị của biến color bằng thuộc tính của colors
+    this.names = name;
+    this.weights = weight;
+  }
+ run():void {
+  console.log(this.colors + this.weights)
+ }
+}
+
+const S450 = new Mercedes("wirite" , "s450 maybach", 1200)
+console.log(S450)
+class Cours {
+ public items : string;
+ private temple: string;
+  cours : number;
+  constructor(color:string , name : string , cours: number){
+    this.items = color;
+    this.temple = name ;
+    this.cours = cours;
+  }
+  add():void {
+    console.log(this.temple)
+  }
+}
+const Car = new Cours("write" , "maybachS500" , 1200)
+// console.log(Car.items)
+console.log(Car.add())
+// Tạo một đối tượng từ lớp Person
+// let person1 = new Person("John", "học viện bưu chính viễn thông");
+// person1.sayHello(); // Output: "Hello, John Doe!"
+
+// person1.sayHello();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+     <p>Tự học Coder</p>
     </div>
   );
 }
